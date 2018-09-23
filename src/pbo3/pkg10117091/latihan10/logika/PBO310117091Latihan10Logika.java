a912a29457a6ef1136314ca968cfe64d6b03d1cb
Dida Handian <handiandida@gmail.com>
2018-09-23 19:52:09 +0700

    Logika

---
+++ src/pbo3/pkg10117091/latihan10/logika/PBO310117091Latihan10Logika.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117091.latihan10.logika;
+
+/* @author
+* NAMA  : DIDA HANDIAN
+* KELAS : PBO3
+* NIM   : 10117091
+* Deskripsi Program : Program ini berisi contoh operator Logika dengan variabel
+*/
+public class PBO310117091Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
